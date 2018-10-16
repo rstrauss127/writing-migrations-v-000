c568fbdd46770cef40c48fb5404ad61fa92405b3
Rebecca Strauss <rstrauss127@gmail.com>
2018-10-16 23:37:54 +0000

    automatically backed up by learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,6 +1,6 @@
 
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[4.2]
   def change
-    change_column(:students, :birthday, :datetime)
+    change_column(:students, :birthdate, :datetime)
   end
 end
